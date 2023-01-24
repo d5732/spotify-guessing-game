cd593fb9cd57fbe198f3e5cc594cab8c4183fb24
Thomas Tubbs <jacksknightemail@gmail.com>
2023-01-24 10:27:39 -0600

    wip: howler.js working

--- src/components/guess/PlayAudio.js
+++ src/components/guess/PlayAudio.js
@@ -1,16 +1,30 @@
 import React from "react";
 import styled from "styled-components";
+import { Howl, Howler } from "howler";
 
 const PlayButton = styled.button`
     padding: 1rem;
-`
+`;
 
-const PlayAudio = ({mp3}) => {
+const PlayAudio = ({ mp3 }) => {
+    console.log("mp3", mp3);
+    const handleClick = () => {
+        var sound = new Howl({
+            src: [mp3],
+            format: ["mp3"],
+            volume: 0.01,
+        });
+        console.log("sound", sound);
+        sound.load();
+        sound.play();
+    };
+
     return (
-        <PlayButton> 
-            <audio controls>
+        <PlayButton onClick={handleClick}>
+            {/* <audio controls>
                 <source src={mp3} type="audio/mp3" />
-            </audio> 
+            </audio>  */}
+            button
         </PlayButton>
     );
 };

94b59e3a35e90b141e832d8e33ae1a3f5d78a227
Thomas Tubbs <jacksknightemail@gmail.com>
2023-01-24 11:21:25 -0600

    wip: fixed play buttons

--- src/components/guess/PlayAudio.js
+++ src/components/guess/PlayAudio.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { Howl, Howler } from "howler";
 
@@ -7,25 +7,36 @@ const PlayButton = styled.button`
 `;
 
 const PlayAudio = ({ mp3 }) => {
-    console.log("mp3", mp3);
-    const handleClick = () => {
-        var sound = new Howl({
+    const [id, setId] = useState("");
+    const [sound, setSound] = useState(
+        new Howl({
             src: [mp3],
             format: ["mp3"],
             volume: 0.01,
-        });
-        console.log("sound", sound);
-        sound.load();
-        sound.play();
+        })
+    );
+    const [playing, setPlaying] = useState(false);
+
+    const handlePlay = () => {
+        setPlaying(true);
+        setId(sound.play());
+    };
+    const handlePause = (id) => {
+        setPlaying(false);
+        sound.pause(id);
     };
 
+    console.log("Play audio re-rendered", id);
     return (
-        <PlayButton onClick={handleClick}>
-            {/* <audio controls>
-                <source src={mp3} type="audio/mp3" />
-            </audio>  */}
-            button
-        </PlayButton>
+        <>
+            <button onClick={() => console.log("boolean", playing, "id", id)}>
+                debug
+            </button>
+            {!playing && <PlayButton onClick={handlePlay}>play</PlayButton>}
+            {playing && (
+                <PlayButton onClick={() => handlePause(id)}>pause</PlayButton>
+            )}
+        </>
     );
 };
 
